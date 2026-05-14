57130de5c821667aabef36095c65875408b555a0
Faith <felominofaith@gmail.com>
2026-05-14 10:15:03 +0800

    gpu

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const products = [
-    { id: 1, name: "Coffee", price: 5.00 },
-    { id: 2, name: "Sandwich", price: 8.50 },
-    { id: 3, name: "Cookie", price: 2.00 },
+    { id: 1, name: "COLORFUL IGame GeForce RTX 5050 Ultra", price: 19590, img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSBin4NQTFLWY39Q6iya-ISuC0pyuKAY2cSweBDPeiHn10KMwSOrTXwIqTOkJYnZwYkkiPFfYoR8Eq7N6KUw01y0-6ct1Pn"  },
+    { id: 2, name: "Gigabyte Rtx 5060 Windforce Max Oc 8gb Gddr7 Graphic Card", price: 22500, img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRzCqX_HsP2rGD1JQd2MQcqv5Qh7mL2Htepxzls_Mj6HSHZhmu4-XtsX_Cdwc_k4sIHDKr42GH_bAqMPbn59QOLn890aE5r"  },
+    { id: 3, name: "Cookie", price: 2.00, img: ""  },
     { id: 4, name: "Bread", price: 4.00, img: "" }
 ];
 
